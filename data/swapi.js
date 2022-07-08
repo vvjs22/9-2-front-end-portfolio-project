f1744593afc5983babea370db101d1524f47ba42
jd richards <jdrichardstech@gmail.com>
2022-07-08 12:19:43 -0400

    initial commit

---
+++ data/swapi.js
@@ -0,0 +1,302 @@
+const results = [
+  {
+    name: 'Luke Skywalker',
+    id: 'L3kdddkek',
+    height: '172',
+    mass: '77',
+    hair_color: 'blond',
+    skin_color: 'fair',
+    eye_color: 'blue',
+    birth_year: '19BBY',
+    gender: 'male',
+    homeworld: {
+      name: 'Tatooine',
+      rotation_period: '23',
+      orbital_period: '304',
+      diameter: '10465',
+      climate: 'arid'
+    },
+    films: [
+      'A New Hope',
+      'The Empire Strikes Back',
+      'Return of the Jedi',
+      'Revenge of the Sith'
+    ],
+    species: [],
+    vehicles: [
+      'https://swapi.dev/api/vehicles/14/',
+      'https://swapi.dev/api/vehicles/30/'
+    ],
+    starships: [
+      'https://swapi.dev/api/starships/12/',
+      'https://swapi.dev/api/starships/22/'
+    ],
+    created: '2011-12-09T13:50:51.644000Z',
+    edited: '2014-12-20T21:17:56.891000Z',
+    url: 'https://swapi.dev/api/people/1/'
+  },
+  {
+    name: 'C-3PO',
+    id: 'Idkk99dkc',
+    height: '167',
+    mass: '75',
+    hair_color: 'n/a',
+    skin_color: 'gold',
+    eye_color: 'yellow',
+    birth_year: '112BBY',
+    gender: 'n/a',
+    homeworld: {
+      name: 'Tatoine',
+      rotation_period: '23',
+      orbital_period: '304',
+      diameter: '10465',
+      climate: 'arid'
+    },
+    films: [
+      'A New Hope',
+      'The Empire Strikes Back',
+      'Return of the Jedi',
+      'The Phantom Menace',
+      'Attack of the Clones',
+      'Revenge of the Sith'
+    ],
+    species: ['Droid'],
+    vehicles: [],
+    starships: [],
+    created: '2012-12-10T15:10:51.357000Z',
+    edited: '2014-12-20T21:17:50.309000Z',
+    url: 'https://swapi.dev/api/people/2/'
+  },
+  {
+    name: 'R2-D2',
+    id: 'Kekk39dis',
+    height: '96',
+    mass: '32',
+    hair_color: 'n/a',
+    skin_color: 'white, blue',
+    eye_color: 'red',
+    birth_year: '33BBY',
+    gender: 'n/a',
+    homeworld: {
+      name: 'Naboo',
+      rotation_period: '26',
+      orbital_period: '312',
+      diameter: '12120',
+      climate: 'temperate'
+    },
+    films: [
+      'A New Hope',
+      'The Empire Strikes Back',
+      'Return of the Jedi',
+      'The Phantom Menace',
+      'Attack of the Clones',
+      'Revenge of the Sith'
+    ],
+    species: ['Droid'],
+    vehicles: [],
+    starships: [],
+    created: '2014-12-10T15:11:50.376000Z',
+    edited: '2014-12-20T21:17:50.311000Z',
+    url: 'https://swapi.dev/api/people/3/'
+  },
+  {
+    name: 'Darth Vader',
+    id: 'O894iriie',
+    height: '202',
+    mass: '136',
+    hair_color: 'none',
+    skin_color: 'white',
+    eye_color: 'yellow',
+    birth_year: '41.9BBY',
+    gender: 'male',
+    homeworld: {
+      name: 'Tatooine',
+      rotation_period: '23',
+      orbital_period: '304',
+      diameter: '10465',
+      climate: 'arid'
+    },
+    films: [
+      'A New Hope',
+      'The Empire Strikes Back',
+      'Return of the Jedi',
+      'Revenge of the Sith'
+    ],
+    species: [],
+    vehicles: [],
+    starships: ['https://swapi.dev/api/starships/13/'],
+    created: '2012-12-10T15:18:20.704000Z',
+    edited: '2014-12-20T21:17:50.313000Z',
+    url: 'https://swapi.dev/api/people/4/'
+  },
+  {
+    name: 'Leia Organa',
+    id: 'Viie78949',
+    height: '150',
+    mass: '49',
+    hair_color: 'brown',
+    skin_color: 'light',
+    eye_color: 'brown',
+    birth_year: '19BBY',
+    gender: 'female',
+    homeworld: {
+      name: 'Alderaan',
+      rotation_period: '24',
+      orbital_period: '364',
+      diameter: '12500',
+      climate: 'temperate'
+    },
+    films: [
+      'A New Hope',
+      'The Empire Strikes Back',
+      'Return of the Jedi',
+      'Revenge of the Sith'
+    ],
+    species: [],
+    vehicles: ['https://swapi.dev/api/vehicles/30/'],
+    starships: [],
+    created: '2009-12-10T15:20:09.791000Z',
+    edited: '2014-12-20T21:17:50.315000Z',
+    url: 'https://swapi.dev/api/people/5/'
+  },
+  {
+    name: 'Owen Lars',
+    id: 'ErT8dk4k3',
+    height: '178',
+    mass: '120',
+    hair_color: 'brown, grey',
+    skin_color: 'light',
+    eye_color: 'blue',
+    birth_year: '52BBY',
+    gender: 'male',
+    homeworld: {
+      name: 'Tatooine',
+      rotation_period: '23',
+      orbital_period: '304',
+      diameter: '10465',
+      climate: 'arid'
+    },
+    films: ['A New Hope', 'Attack of the Clones', 'Revenge of the Sith'],
+    species: [],
+    vehicles: [],
+    starships: [],
+    created: '2016-12-10T15:52:14.024000Z',
+    edited: '2014-12-20T21:17:50.317000Z',
+    url: 'https://swapi.dev/api/people/6/'
+  },
+  {
+    name: 'Beru Whitesun Lars',
+    id: 'Ckdkke9d3',
+    height: '165',
+    mass: '75',
+    hair_color: 'brown',
+    skin_color: 'light',
+    eye_color: 'blue',
+    birth_year: '47BBY',
+    gender: 'female',
+    homeworld: {
+      name: 'Tatooine',
+      rotation_period: '23',
+      orbital_period: '304',
+      diameter: '10465',
+      climate: 'arid'
+    },
+    films: ['A New Hope', 'Attack of the Clones', 'Revenge of the Sith'],
+    species: [],
+    vehicles: [],
+    starships: [],
+    created: '2018-12-10T15:53:41.121000Z',
+    edited: '2014-12-20T21:17:50.319000Z',
+    url: 'https://swapi.dev/api/people/7/'
+  },
+  {
+    name: 'R5-D4',
+    id: 'Qwfkr988d',
+    height: '97',
+    mass: '32',
+    hair_color: 'n/a',
+    skin_color: 'white, red',
+    eye_color: 'red',
+    birth_year: 'unknown',
+    gender: 'n/a',
+    homeworld: {
+      name: 'Tatooine',
+      rotation_period: '23',
+      orbital_period: '304',
+      diameter: '10465',
+      climate: 'arid'
+    },
+    films: ['A New Hope'],
+    species: ['Droid'],
+    vehicles: [],
+    starships: [],
+    created: '2020-12-10T15:57:50.959000Z',
+    edited: '2014-12-20T21:17:50.321000Z',
+    url: 'https://swapi.dev/api/people/8/'
+  },
+  {
+    name: 'Biggs Darklighter',
+    id: 'Vidkel898',
+    height: '183',
+    mass: '84',
+    hair_color: 'black',
+    skin_color: 'light',
+    eye_color: 'brown',
+    birth_year: '24BBY',
+    gender: 'male',
+    homeworld: {
+      name: 'Tatooine',
+      rotation_period: '23',
+      orbital_period: '304',
+      diameter: '10465',
+      climate: 'arid'
+    },
+    films: ['A New Hope'],
+    species: [],
+    vehicles: [],
+    starships: ['https://swapi.dev/api/starships/12/'],
+    created: '2011-12-10T15:59:50.509000Z',
+    edited: '2014-12-20T21:17:50.323000Z',
+    url: 'https://swapi.dev/api/people/9/'
+  },
+  {
+    name: 'Obi-Wan Kenobi',
+    id: 'Ghfjkd998',
+    height: '182',
+    mass: '77',
+    hair_color: 'auburn, white',
+    skin_color: 'fair',
+    eye_color: 'blue-gray',
+    birth_year: '57BBY',
+    gender: 'male',
+    homeworld: {
+      name: 'Stewjon',
+      rotation_period: 'unknown',
+      orbital_period: 'unknown',
+      diameter: '0',
+      climate: 'temperate'
+    },
+    films: [
+      'A New Hope',
+      'The Empire Strikes Back',
+      'Return of the Jedi',
+      'The Phantom Menace',
+      'Attack of the Clones',
+      'Revenge of the Sith'
+    ],
+    species: [],
+    vehicles: ['https://swapi.dev/api/vehicles/38/'],
+    starships: [
+      'https://swapi.dev/api/starships/48/',
+      'https://swapi.dev/api/starships/59/',
+      'https://swapi.dev/api/starships/64/',
+      'https://swapi.dev/api/starships/65/',
+      'https://swapi.dev/api/starships/74/'
+    ],
+    created: '2014-12-10T16:16:29.192000Z',
+    edited: '2014-12-20T21:17:50.325000Z',
+    url: 'https://swapi.dev/api/people/10/'
+  }
+];
+
+module.exports = results;
---
+++ index.js
@@ -0,0 +1,99 @@
+const characters = require('./data/swapi');
+
+/**
+ * listAllCharacters()
+ * --------------------
+ * Returns all character names from an array of characters. If the inputted array is empty, returns 0.
+ * @param {Object[] characters} - an array of movie characters
+ * @returns {string[]} an array of strings, which are character names
+ */
+
+function listAllCharacters() {}
+
+//UnComment line 15 and line 17 below to test the function
+// should return 0
+// console.log(listAllCharacters([]));
+// should return ['Luke Skywalker','C-3PO','R2-D2','Darth Vader','Leia Organa','Owen Lars','Beru Whitesun Lars','R5-D4','Biggs Darklighter','Obi-Wan Kenobi']
+// console.log(listAllCharacters(characters));
+
+/**
+ * averageHeightOfAllCharacters()
+ * -------------------------------
+ * Returns the average height of all of the listed characters
+ * @param {Object[] characters} - an array of movie characters
+ * @returns {number} - a number representing the average height
+ *
+ */
+
+function averageHeightOfAllCharacters() {}
+
+//Uncomment the line below to test the function
+//console.log(averageHeightOfAllCharacters(characters))
+
+/**
+ * countByEyeColor()
+ * -------------------
+ * Returns an object where the keys are the eye colors and the values are number of characters with that eye color. If the inputted array is empty, return 'Nothing to check.'
+ * @param {Object[] characters} - an array of movie characters
+ * @returns {Object} - keys are the eye colors and values are the number of characters with that eye color
+ */
+
+function countByEyeColor(chars) {}
+
+// should return 'Nothing to check.'
+// console.log(countByEyeColor([]))
+// should return { blue: 3, yellow: 2, red: 2, brown: 2, 'blue-gray': 1 }
+// console.log(countByEyeColor(characters));
+
+/**
+ * getAllCharactersCreatedAfterYear()
+ * ------------------
+ * Returns an array of all characters created on or after a certain year
+ * @param {Object[] characters} - an array of movie characters
+ * @param {Number} date - date to be considered for when character object was created
+ * @returns {String[]} - returns an array of character names
+ * EXAMPLE:
+ * getAllCharactersCreatedAfterYear(characters, 2014)
+ *
+ * [
+  'R2-D2',
+  'Owen Lars',
+  'Beru Whitesun Lars',
+  'R5-D4',
+  'Obi-Wan Kenobi'
+ * ]
+ * 
+ * 
+ *
+ */
+
+function getAllCharactersCreatedAfterYear(chars, d) {}
+
+// UnComment line below to test
+// console.log(getAllCharactersCreatedAfterYear(characters, 2014));
+
+/** getCharactersInMovie()
+ *  ------------------
+ *  Returns an object of characters who were in a certain film. Their id will be the key and their name should be the value. Title should be CASE INCENSITIVE
+ *  @param {Object[] characters} - an array of movie characters
+ *  @param {String} - string with the title of the movie
+ *  @returns {Object} - key should be the id and the value should be their name
+ *
+ * EXAMPLE:
+ *
+ * getCharacterIds(characters, 'return of the jedi')
+ * 
+ * //> {
+   L3kdddkek: 'Luke Skywalker',
+   Idkk99dkc: 'C-3PO',
+   Kekk39dis: 'R2-D2',
+   O894iriie: 'Darth Vader',
+   Viie78949: 'Leia Organa',
+   Ghfjkd998: 'Obi-Wan Kenobi'
+   }
+ */
+
+function getCharactersInMovie(chars, title) {}
+
+// Uncomment line below to test
+// console.log(getCharactersInMovie(characters, 'return of the jedi'));
